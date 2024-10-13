e28de408dc12996f1bad31318eb62425f1dc03be
Lokesh Badela <lakkubaddu1117@gmail.com>
2024-10-13 18:34:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express			 = require("express");
+var methodOverride   = require("method-override");
+var bodyParser       = require("body-parser");
+var mongoose         = require("mongoose");
+var indexRoutes      = require("./routes/index.js");
+var practiceRoutes      = require("./routes/practice.js");
+var interviewRoutes      = require("./routes/interview.js");
+var signinRoutes = require("./routes/signin.js");
+var signupRoutes = require("./routes/signup.js");
+
+var app = express();
+app.use(express.static('public'))
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost/acadshare");
+app.use("/",indexRoutes);
+app.use("/practice",practiceRoutes);
+app.use("/interviews",interviewRoutes);
+app.use("/signin",signinRoutes);
+app.use("/signup",signupRoutes);
+
+app.listen(3000,'localhost',function(){
+	console.log("server has started");
+});
